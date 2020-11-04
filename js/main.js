fc982927797d5b31a0e62ac8bd14e15b950d42ba
alessio <alessio.cirillo.1995@gmail.com>
2020-11-04 15:55:59 +0100

    print to screen

--- js/main.js
+++ js/main.js
@@ -1,99 +1,129 @@
-// Icon set
-const icons = [
-    {
-      name: 'cat',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'crow',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dog',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dove',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'dragon',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'horse',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'hippo',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'fish',
-      prefix: 'fa-',
-      type: 'animal',
-      family: 'fas',
-    },
-    {
-      name: 'carrot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'apple-alt',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'lemon',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'pepper-hot',
-      prefix: 'fa-',
-      type: 'vegetable',
-      family: 'fas',
-    },
-    {
-      name: 'user-astronaut',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-graduate',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-ninja',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-    {
-      name: 'user-secret',
-      prefix: 'fa-',
-      type: 'user',
-      family: 'fas',
-    },
-];
+
+$(document).ready(function(){
+
+    // Icon set
+    const icons = [
+        {
+          name: 'cat',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'crow',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dog',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dove',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'dragon',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'horse',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'hippo',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'fish',
+          prefix: 'fa-',
+          type: 'animal',
+          family: 'fas',
+        },
+        {
+          name: 'carrot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'apple-alt',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'lemon',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'pepper-hot',
+          prefix: 'fa-',
+          type: 'vegetable',
+          family: 'fas',
+        },
+        {
+          name: 'user-astronaut',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-graduate',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-ninja',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+        {
+          name: 'user-secret',
+          prefix: 'fa-',
+          type: 'user',
+          family: 'fas',
+        },
+    ];
+    console.log(icons);
+
+    //ICONS CONTAINER
+    const container = $('icons');
+
+    //PRINT ON SCREEN
+    printToScreen(icons, container);
+
+});//----end ready
+
+
+//FUNCTION PRINT TO SCREEN
+function printToScreen(icons, container){
+
+    icons.forEach(icon => {
+        
+        const {family, prefix, name} = icon;
+
+        const html = 
+        `<div class="icon">
+            <i class="${family} ${prefix}${name}"></i>
+            <div class="title">${name}</div>
+        </div>`;
+
+        container.append(html);
+    });
+}
